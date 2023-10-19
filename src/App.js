c6e25eccf605bd8870276405ff4c4731cf7e41b2
beanbeeean <beanbeeean@naver.com>
2023-10-19 11:48:11 +0900

    connetion

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  // 연결된거 확인
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("/showMe")
+      .then((res) => {
+        return res.json();
+      })
+      .then(function (result) {
+        setData(result);
+      });
+  }, []);
+  return (
+    <div>
+      Home
+      <ul>
+        {data.map((v, idx) => (
+          <li key={`${idx}-${v}`}>{v}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Home;
