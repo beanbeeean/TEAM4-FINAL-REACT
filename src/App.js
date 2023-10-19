7371c117c8131641817b255944206c679c5b75a6
siyeong1013 <tldud4346@gmail.com>
2023-10-19 12:01:28 +0900

    [TF-61]-Header_Nav_Footer-구성

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Header from "./components/include/Header";
+import Footer from "./components/include/Footer";
+import Nav from "./components/include/Nav";
 
 function App() {
   return (
     <div>
+      <Header />
+      <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
