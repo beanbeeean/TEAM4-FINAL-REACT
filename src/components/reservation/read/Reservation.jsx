9b9eeff0b639daac95587e050ea8b5e92b9d4f5e
beanbeeean <beanbeeean@naver.com>
2023-10-20 22:52:54 +0900

    NewUI -ing2

--- src/components/reservation/read/Reservation.jsx
+++ src/components/reservation/read/Reservation.jsx
@@ -1,16 +1,19 @@
-import React, { useState } from 'react';
-import { Button, Container, Row, Col, Card } from 'react-bootstrap';
-import ReservationModal from './ReservationModal';
+import React, { useState } from "react";
+import { Button, Container, Row, Col, Card } from "react-bootstrap";
+import ReservationModal from "./ReservationModal";
 
-const seats = Array.from({ length: 20 }).map((_, i) => ({ id: i + 1, reserved: false }));
+const seats = Array.from({ length: 20 }).map((_, i) => ({
+  id: i + 1,
+  reserved: false,
+}));
 
 function Reservation() {
   const [selectedSeat, setSelectedSeat] = useState(null);
   const [modalShow, setModalShow] = useState(false);
   const [selectedSeat1, setSelectedSeat1] = useState(null);
   const [currentTime, setCurrentTime] = useState(new Date());
-  
-  const [seat, setSeat] = useState([2,6,8,7]);
+
+  const [seat, setSeat] = useState([2, 6, 8, 7]);
 
   const handleSeatClick = (seat) => {
     if (!seat.reserved) {
@@ -31,22 +34,26 @@ function Reservation() {
   };
 
   return (
-    <Container style={{ maxWidth: '600px', marginTop: '50px' }}>
+    <Container style={{ maxWidth: "600px", marginTop: "50px" }}>
       <Card className="p-4 shadow-sm">
         <h2 className="text-center mb-4">독서실 예약</h2>
         <Row className="mb-4">
-          {seats.map((seat) => ( 
+          {seats.map((seat) => (
             <Col xs={2} className="mb-2" key={seat.id}>
-              <div 
+              <div
                 style={{
-                  display: 'flex',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  backgroundColor: seat.reserved ? '#ddd' : selectedSeat === seat.id ? '#fd8a69' : '#FECCBE',
-                  borderRadius: '8px',
-                  cursor: seat.reserved ? 'not-allowed' : 'pointer',
-                  transition: 'background-color 0.3s',
-                  height: '40px'
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  backgroundColor: seat.reserved
+                    ? "#ddd"
+                    : selectedSeat === seat.id
+                    ? "#fd8a69"
+                    : "#FECCBE",
+                  borderRadius: "8px",
+                  cursor: seat.reserved ? "not-allowed" : "pointer",
+                  transition: "background-color 0.3s",
+                  height: "40px",
                 }}
                 onClick={() => handleSeatClick(seat)}
               >
@@ -55,16 +62,24 @@ function Reservation() {
             </Col>
           ))}
         </Row>
-        <Button 
-          onClick={()=>handleButtonClick(selectedSeat)} 
+        <Button
+          onClick={() => handleButtonClick(selectedSeat)}
           disabled={!selectedSeat}
           block
-          style={{backgroundColor:selectedSeat ? '#fd8a69' : '#a0a0a0',border:'none'}}
+          style={{
+            backgroundColor: selectedSeat ? "#fd8a69" : "#a0a0a0",
+            border: "none",
+          }}
         >
           예약하기
         </Button>
       </Card>
-      <ReservationModal show={modalShow} no={selectedSeat} time={currentTime} onHide={() => setModalShow(false)} />
+      <ReservationModal
+        show={modalShow}
+        no={selectedSeat}
+        time={currentTime}
+        onHide={() => setModalShow(false)}
+      />
     </Container>
   );
 }
