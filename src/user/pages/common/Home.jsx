c11444de948353260b8d312efaf098b02817e248
siyeong1013 <tldud4346@gmail.com>
2023-10-24 10:05:55 +0900

    [TF-84] user search result 화면 구성

--- src/user/pages/common/Home.jsx
+++ src/user/pages/common/Home.jsx
@@ -127,19 +127,19 @@ const Home = () => {
             <div className={styles.bar}>
               <div className={styles.barLabel}>1열람실</div>
               <div className={styles.barFill} style={{ width: "70%" }}>
-                <span className={styles.barValue}>70%</span>
+                <span className={styles.barValue}>35</span>
               </div>
             </div>
             <div className={styles.bar}>
               <div className={styles.barLabel}>2열람실</div>
               <div className={styles.barFill} style={{ width: "50%" }}>
-                <span className={styles.barValue}>50%</span>
+                <span className={styles.barValue}>22</span>
               </div>
             </div>
             <div className={styles.bar}>
               <div className={styles.barLabel}>3열람실</div>
               <div className={styles.barFill} style={{ width: "90%" }}>
-                <span className={styles.barValue}>90%</span>
+                <span className={styles.barValue}>40</span>
               </div>
             </div>
           </div>
