807977a52203d7ecb0c9c93009329c8bd9557724
beanbeeean <beanbeeean@naver.com>
2023-10-25 20:08:12 +0900

    TF-91_Admin_Reservation_Management -ing

--- src/admin/components/reservation/ReservationManagement.jsx
+++ src/admin/components/reservation/ReservationManagement.jsx
@@ -1,9 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import stylesAdmin from "../../css/reservation/ReservationManagement.module.css";
+import ReadRoomSetting from "./ReadRoomSetting";
+import ReadRoomReservationList from "./ReadRoomReservationList";
+import StudyRoomReservationList from "./StudyRoomReservationList";
 
 const ReservationManagement = () => {
+  const [tapActive, setTapActive] = useState(true);
+  const [smallTap, setSmallTap] = useState(true);
+
+  const tapChangeHandler = (num) => {
+    if (num == 1) {
+      setTapActive(true);
+      setSmallTap(true);
+    } else if (num == 2) {
+      setTapActive(false);
+      setSmallTap(true);
+    }
+  };
   return (
-    <div className={stylesAdmin.management_wrap}>ReservationManagement</div>
+    <div className={stylesAdmin.management_wrap}>
+      <h2 className={stylesAdmin.admin_title}>RESERVATION MANAGEMENT</h2>
+      <ul className={stylesAdmin.rm_tap}>
+        <li
+          onClick={() => tapChangeHandler(1)}
+          className={`${tapActive && stylesAdmin.rm_tap_on}`}
+        >
+          예약 내역
+        </li>
+        <li
+          onClick={() => tapChangeHandler(2)}
+          className={`${!tapActive && stylesAdmin.rm_tap_on}`}
+        >
+          설정
+        </li>
+      </ul>
+      <ul className={stylesAdmin.rm_first_tap}>
+        <li
+          onClick={() => setSmallTap(true)}
+          className={`${smallTap && stylesAdmin.rm_tap_on}`}
+        >
+          · 열람실
+        </li>
+        <li
+          onClick={() => setSmallTap(false)}
+          className={`${!smallTap && stylesAdmin.rm_tap_on}`}
+        >
+          · 스터디룸
+        </li>
+      </ul>
+      {/* 컴포넌트 나눠서 생성해야함 */}
+      <div className={stylesAdmin.rm_content}>
+        {tapActive ? (
+          smallTap ? (
+            <ReadRoomReservationList />
+          ) : (
+            <StudyRoomReservationList />
+          )
+        ) : smallTap ? (
+          <ReadRoomSetting />
+        ) : (
+          ""
+        )}
+      </div>
+    </div>
   );
 };
 
---
+++ src/admin/components/reservation/StudyRoomSetting.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const StudyRoomSetting = () => {
+  // NO, 스터디룸 이름, 가격, 예약 가능 여부
+  return <div>StudyRoomManagement</div>;
+};
+
+export default StudyRoomSetting;
