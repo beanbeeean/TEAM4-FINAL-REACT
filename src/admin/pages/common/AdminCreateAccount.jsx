289ec0294b31360783b37a09443d51345efedaad
siyeong1013 <tldud4346@gmail.com>
2023-11-06 12:45:49 +0900

    [TF-43] Admin SignUp

--- src/admin/pages/common/AdminCreateAccount.jsx
+++ src/admin/pages/common/AdminCreateAccount.jsx
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import stylesAdmin from "../../css/common/AdminCreateAccount.module.css";
 import { Link } from "react-router-dom";
+import { PiWarningCircleFill } from "react-icons/pi";
 
 const AdminCreateAccount = () => {
+  const [password1, setPassword1] = useState("");
+  const [password2, setPassword2] = useState("");
+  const [passwordMatch, setPasswordMatch] = useState(true);
+
+  const handleSubmit = (password1, password2) => {
+    if (password1 === password2) {
+      setPasswordMatch(true);
+    } else {
+      setPasswordMatch(false);
+    }
+  };
+
+  console.log("passwordMatch :: ", passwordMatch);
+
   return (
     <div className={stylesAdmin.admin_login_wrap}>
       <div className={stylesAdmin.login_container}>
@@ -16,10 +31,30 @@ const AdminCreateAccount = () => {
           <input type="text" name="id" placeholder="ID" />
           <input
             type="password"
-            name="password"
+            name="password1"
             placeholder="Passsword"
             id="pwd"
+            onChange={(e) => setPassword1(e.target.value)}
           />
+          <input
+            type="password"
+            name="password2"
+            placeholder="Verify Passsword"
+            id="pwd"
+            onChange={(e) => {
+              setPassword2(e.target.value);
+              handleSubmit(password1, e.target.value);
+            }}
+          />
+
+          {!passwordMatch && (
+            <div className={stylesAdmin.error_message}>
+              <PiWarningCircleFill style={{ color: "red" }} />
+              &nbsp;Passwords do not match.
+            </div>
+          )}
+
+          <input type="text" name="name" placeholder="Name" />
 
           <input
             type="submit"
