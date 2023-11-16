35ff2fa33659614108e8c4eb29bb3d76a5433078
beanbeeean <beanbeeean@naver.com>
2023-11-16 17:52:16 +0900

    testing

--- src/user/pages/common/Home.jsx
+++ src/user/pages/common/Home.jsx
@@ -119,7 +119,8 @@ const Home = () => {
   }, []);
 
   useEffect(() => {
-    setBooks(bookDto.filter((e) => e.b_category === 3));
+    // setBooks(bookDto.filter((e) => e.b_category === 3));
+    setBooks(bookDto);
   }, [bookDto]);
 
   useEffect(() => {
