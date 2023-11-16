44e8ba3f9c456e090bd7668e1d2e63498c25fc85
beanbeeean <beanbeeean@naver.com>
2023-11-16 20:56:45 +0900

    main4

--- src/authorization/Authorization.jsx
+++ src/authorization/Authorization.jsx
@@ -23,100 +23,93 @@ import { logout } from "../user/components/common/login/APIUtils";
 import { userLogout } from "../redux/user/slices/userSlice";
 
 const Authorization = () => {
-  
   const authorization = useSelector((state) => state.user.userDto.u_role);
 
   const dispatch = useDispatch();
 
   const admin = () => {
-    if(authorization==="ROLE_USER"){
+    if (authorization === "ROLE_USER") {
       logout()
-      .then(response => {
-        dispatch(userLogout())
-      })
-      .catch(error => {
-      });
-    } else if(authorization==="ROLE_ADMIN" || authorization==="ROLE_SUPER"){
+        .then((response) => {
+          dispatch(userLogout());
+        })
+        .catch((error) => {});
+    } else if (
+      authorization === "ROLE_ADMIN" ||
+      authorization === "ROLE_SUPER"
+    ) {
       return true;
     }
     return false;
-  }
+  };
 
   const user = () => {
-    if(authorization==="ROLE_ADMIN" || authorization==="ROLE_SUPER"){
+    if (authorization === "ROLE_ADMIN" || authorization === "ROLE_SUPER") {
       logout()
-      .then(response => {
-        dispatch(userLogout())
-      })
-      .catch(error => {
-      });
-    } else if(authorization==="ROLE_USER"){
+        .then((response) => {
+          dispatch(userLogout());
+        })
+        .catch((error) => {});
+    } else if (authorization === "ROLE_USER") {
       return true;
     }
     return false;
-  }
+  };
 
   return (
     <>
-    {/* <Header />
-      <Nav />*/}
-          {/* <Header />
-      <Nav />*/}
-          {window.location.pathname.includes("/admin") ? (
-            <>
+      {window.location.pathname.includes("/admin") ? (
+        <>
+          <Routes>
+            <Route path="/admin/login" element={<AdminLogin />} />
+            <Route
+              path="/admin/create_account"
+              element={<AdminCreateAccount />}
+            />
+            <Route
+              path="/admin/management"
+              element={admin() ? <Admin /> : <AdminLogin />}
+            />
+          </Routes>
+        </>
+      ) : (
+        <div id="user_wrap">
+          <Sidebar />
+          <div id="section">
+            <ChatModal />
+            <Header />
+            <div className="section_area">
               <Routes>
-                <Route path="/admin/login" element={<AdminLogin />} />
+                <Route path="/" element={user() ? <Home /> : <Home />} />
+                {/* <Route path="/books" element={<BookList />} /> */}
+                {/* <Route path="/books/:id" element={<BookDetail />} /> */}
+                <Route path="/community" element={<Community />} />
+                <Route path="/community/:id" element={<CommunityDetail />} />
+                <Route path="/community_write" element={<CommunityWrite />} />
                 <Route
-                  path="/admin/create_account"
-                  element={<AdminCreateAccount />}
+                  path="/community_modify/:id"
+                  element={<CommunityModify />}
                 />
-                <Route path="/admin/management" element={admin() ? <Admin /> : <AdminLogin />}  />
+
+                <Route path="/checkout_books" element={<CheckoutList />} />
+                <Route
+                  path="/checkout_books/:id"
+                  element={<CheckoutDetail />}
+                />
+                <Route path="/mypage" element={<MyPage />} />
+                <Route path="/reservation" element={<Reservation />} />
+
+                <Route path="/search/:keyword" element={<SearchMain />} />
+
+                <Route
+                  path="/oauth2/redirect"
+                  element={<OAuth2RedirectHandler />}
+                ></Route>
               </Routes>
-            </>
-          ) : (
-            <div id="user_wrap">
-              <Sidebar />
-              <div id="section">
-                <ChatModal />
-                <Header />
-                <div className="section_area">
-                  <Routes>
-                    <Route path="/" element={user() ? <Home />:<Home />} />
-                    {/* <Route path="/books" element={<BookList />} /> */}
-                    {/* <Route path="/books/:id" element={<BookDetail />} /> */}
-                    <Route path="/community" element={<Community />} />
-                    <Route
-                      path="/community/:id"
-                      element={<CommunityDetail />}
-                    />
-                    <Route
-                      path="/community_write"
-                      element={<CommunityWrite />}
-                    />
-                    <Route
-                      path="/community_modify/:id"
-                      element={<CommunityModify />}
-                    />
-
-                    <Route path="/checkout_books" element={<CheckoutList />} />
-                    <Route
-                      path="/checkout_books/:id"
-                      element={<CheckoutDetail />}
-                    />
-                    <Route path="/mypage" element={<MyPage />} />
-                    <Route path="/reservation" element={<Reservation />} />
-
-                    <Route path="/search/:keyword" element={<SearchMain />} />
-
-                    <Route
-                      path="/oauth2/redirect"
-                      element={<OAuth2RedirectHandler />}
-                    ></Route>
-                  </Routes>
-                </div>
-              </div>
             </div>
-          )}
+          </div>
+        </div>
+      )}
     </>
   );
 };
